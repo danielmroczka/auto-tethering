9389b0e27524cd862624ebcfb41e8099dfa992d2
daniel <daniel.mroczka@gmail.com>
2015-09-29 20:10:51 +0200

    Cleanup

--- app/src/main/java/com/labs/dm/auto_tethering/BootCompletedReceiver.java
+++ app/src/main/java/com/labs/dm/auto_tethering/BootCompletedReceiver.java
@@ -5,8 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
-    public BootCompletedReceiver() {
-    }
 
     @Override
     public void onReceive(Context context, Intent intent) {
