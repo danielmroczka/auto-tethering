725642edb347fc1722bac6acf8a924caa07294a0
daniel <daniel.mroczka@gmail.com>
2015-10-07 03:52:30 +0200

    Refactoring main activity

--- app/src/main/java/com/labs/dm/auto_tethering/AppProperties.java
+++ app/src/main/java/com/labs/dm/auto_tethering/AppProperties.java
@@ -1,140 +1,20 @@
 package com.labs.dm.auto_tethering;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
-
 /**
  * Created by daniel on 2015-09-29.
  */
 public class AppProperties {
 
-    private static final String ACTIVATE_ON_STARTUP = "activate.on.startup";
-    private static final String ACTIVATE_3G = "activate.3g";
-    private static final String ACTIVATE_TETHERING = "activate.tethering";
-    private static final String SCHEDULER = "scheduler.on";
-    private static final String TIME_OFF = "time.off";
-    private static final String TIME_ON = "time.on";
-    private static final String ACTIVATE_ON_PHONE_NUMBER = "activate.only.on.phone.number";
-    private static final String PHONE_NUMBER = "activated.phone.number";
-    private static final String SIMCARD = "sim.card";
-    private static final String LATEST_VERSION = "latest.version";
+    public static final String ACTIVATE_ON_STARTUP = "activate.on.startup";
+    public static final String ACTIVATE_3G = "activate.3g";
+    public static final String ACTIVATE_TETHERING = "activate.tethering";
+    public static final String SCHEDULER = "scheduler.on";
+    public static final String TIME_OFF = "time.off";
+    public static final String TIME_ON = "time.on";
+    public static final String ACTIVATE_ON_PHONE_NUMBER = "activate.only.on.phone.number";
+    public static final String PHONE_NUMBER = "activated.phone.number";
+    public static final String SIMCARD = "sim.card";
+    public static final String LATEST_VERSION = "latest.version";
 
-    private SharedPreferences sp;
-    private boolean activateOnStartup;
-    private boolean activate3G;
-    private boolean activateTethering;
-    private boolean scheduler;
-    private boolean activateOnPhoneNumber;
-    private String timeOff;
-    private String timeOn;
-    private String phoneNumber;
-    private String simCard;
-    private String latestVersion;
 
-    public boolean isScheduler() {
-        return scheduler;
-    }
-
-    public void setScheduler(boolean scheduler) {
-        this.scheduler = scheduler;
-    }
-
-    public void load(Context ctx) {
-        sp = PreferenceManager.getDefaultSharedPreferences(ctx);
-        activateOnStartup = sp.getBoolean(AppProperties.ACTIVATE_ON_STARTUP, true);
-        activate3G = sp.getBoolean(AppProperties.ACTIVATE_3G, false);
-        activateTethering = sp.getBoolean(AppProperties.ACTIVATE_TETHERING, true);
-        scheduler = sp.getBoolean(AppProperties.SCHEDULER, false);
-        timeOff = sp.getString(AppProperties.TIME_OFF, "0:00");
-        timeOn = sp.getString(AppProperties.TIME_ON, "6:00");
-        simCard = sp.getString(AppProperties.SIMCARD, "");
-        latestVersion = sp.getString(AppProperties.LATEST_VERSION, "");
-    }
-
-    public void save(Context ctx) {
-        sp = PreferenceManager.getDefaultSharedPreferences(ctx);
-        SharedPreferences.Editor editor = sp.edit();
-        editor.putBoolean(ACTIVATE_ON_STARTUP, activateOnStartup);
-        editor.putBoolean(ACTIVATE_3G, activate3G);
-        editor.putBoolean(ACTIVATE_TETHERING, activateTethering);
-        editor.putBoolean(SCHEDULER, scheduler);
-        editor.putString(TIME_OFF, timeOff);
-        editor.putString(TIME_ON, timeOn);
-        editor.putString(SIMCARD, simCard);
-        editor.putString(LATEST_VERSION, latestVersion);
-        editor.apply();
-    }
-
-    public boolean isActivateOnStartup() {
-        return activateOnStartup;
-    }
-
-    public void setActivateOnStartup(boolean activateOnStartup) {
-        this.activateOnStartup = activateOnStartup;
-    }
-
-    public boolean isActivate3G() {
-        return activate3G;
-    }
-
-    public void setActivate3G(boolean activate3G) {
-        this.activate3G = activate3G;
-    }
-
-    public boolean isActivateTethering() {
-        return activateTethering;
-    }
-
-    public void setActivateTethering(boolean activateTethering) {
-        this.activateTethering = activateTethering;
-    }
-
-    public String getTimeOff() {
-        return timeOff;
-    }
-
-    public void setTimeOff(String timeOff) {
-        this.timeOff = timeOff;
-    }
-
-    public String getTimeOn() {
-        return timeOn;
-    }
-
-    public void setTimeOn(String timeOn) {
-        this.timeOn = timeOn;
-    }
-
-    public boolean isActivateOnPhoneNumber() {
-        return activateOnPhoneNumber;
-    }
-
-    public void setActivateOnPhoneNumber(boolean activateOnPhoneNumber) {
-        this.activateOnPhoneNumber = activateOnPhoneNumber;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getSimCard() {
-        return simCard;
-    }
-
-    public void setSimCard(String simCard) {
-        this.simCard = simCard;
-    }
-
-    public String getLatestVersion() {
-        return latestVersion;
-    }
-
-    public void setLatestVersion(String latestVersion) {
-        this.latestVersion = latestVersion;
-    }
 }
--- app/src/main/java/com/labs/dm/auto_tethering/activity/MainActivity.java
+++ app/src/main/java/com/labs/dm/auto_tethering/activity/MainActivity.java
@@ -1,116 +1,28 @@
 package com.labs.dm.auto_tethering.activity;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.wifi.WifiConfiguration;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 import android.telephony.TelephonyManager;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.labs.dm.auto_tethering.AppProperties;
 import com.labs.dm.auto_tethering.R;
 import com.labs.dm.auto_tethering.service.TetheringService;
 
-public class MainActivity extends Activity {
+public class MainActivity extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener, Preference.OnPreferenceChangeListener {
 
-
-    private CheckBox activateOnStartup;
-    private CheckBox activate3G;
-    private CheckBox activateTethering;
-    private CheckBox scheduler;
-    private CheckBox activeOnSIMCard;
-
-    private EditText switchOffTime;
-    private EditText switchOnTime;
-
-    private TextView ssidText;
-
-    private Button ssidButton;
-
-    private AppProperties props;
-
+    private SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        props = new AppProperties();
-
-        activateOnStartup = (CheckBox) findViewById(R.id.activateOnStartupCheckBox);
-        activate3G = (CheckBox) findViewById(R.id.turnOn3GCheckBox);
-        activateTethering = (CheckBox) findViewById(R.id.turnOnWifiCheckBox);
-        scheduler = (CheckBox) findViewById(R.id.schedulerCheckBox);
-        activeOnSIMCard = (CheckBox) findViewById(R.id.onlyOnPhoneNumberCheckBox);
-
-        switchOffTime = (EditText) findViewById(R.id.switchOffTime);
-        switchOnTime = (EditText) findViewById(R.id.switchOnTime);
-        ssidText = (TextView) findViewById(R.id.ssidText);
-        ssidButton = (Button) findViewById(R.id.button);
-
-        activeOnSIMCard.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                TelephonyManager tMgr = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
-                String simCard = tMgr.getSimSerialNumber();
-                if (simCard == null || simCard.isEmpty()) {
-                    Toast.makeText(getApplicationContext(), "Unable to retrieve SIM Card Serial!", Toast.LENGTH_LONG).show();
-                    activeOnSIMCard.setChecked(false);
-                }
-
-                props.setSimCard(simCard);
-            }
-        });
-
-
-        scheduler.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                switchOffTime.setEnabled(isChecked);
-                switchOnTime.setEnabled(isChecked);
-
-            }
-        });
-
-        ssidButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent tetherSettings = new Intent();
-                tetherSettings.setClassName("com.android.settings", "com.android.settings.TetherSettings");
-                startActivityForResult(tetherSettings, 1);
-            }
-        });
-
-        props.load(getBaseContext());
-        activeOnSIMCard.setChecked(props.getSimCard() != null || !props.getSimCard().isEmpty());
-
-        activateOnStartup.setChecked(props.isActivateOnStartup());
-        activate3G.setChecked(props.isActivate3G());
-        activateTethering.setChecked(props.isActivateTethering());
-        switchOnTime.setText(props.getTimeOn());
-        switchOffTime.setText(props.getTimeOff());
-        scheduler.setChecked(props.isScheduler());
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        switch (requestCode) {
-            case 1: {
-                WifiConfiguration cfg = TetheringService.getWifiApConfiguration(getApplicationContext());
-                ssidText.setText(cfg.SSID);
-                break;
-            }
-        }
+        addPreferencesFromResource(R.xml.preferences);
+        prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
     }
 
     @Override
@@ -118,15 +30,15 @@ public class MainActivity extends Activity {
         super.onPostCreate(savedInstanceState);
         Intent serviceIntent = new Intent(this, TetheringService.class);
         startService(serviceIntent);
-        WifiConfiguration cfg = TetheringService.getWifiApConfiguration(getApplicationContext());
-        ssidText.setText(cfg.SSID);
-        displayPrompt();
+        //WifiConfiguration cfg = TetheringService.getWifiApConfiguration(getApplicationContext());
+        //ssidText.setText(cfg.SSID);
+        //displayPrompt();
     }
 
     private void displayPrompt() {
-        if (props.getLatestVersion() != null || !props.getLatestVersion().isEmpty()) {
-            return;
-        }
+        //if (props.getLatestVersion() != null || !props.getLatestVersion().isEmpty()) {
+        //    return;
+        //}
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
@@ -140,9 +52,9 @@ public class MainActivity extends Activity {
         builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                activate3G.setChecked(true);
-                activateTethering.setChecked(true);
-                persist();
+                //activate3G.setChecked(true);
+                //activateTethering.setChecked(true);
+                //persist();
             }
 
         });
@@ -158,25 +70,38 @@ public class MainActivity extends Activity {
         alert.show();
     }
 
-    private void persist() {
-        props.setActivate3G(activate3G.isChecked());
-        props.setActivateTethering(activateTethering.isChecked());
-        props.setActivateOnStartup(activateOnStartup.isChecked());
-        props.setScheduler(scheduler.isChecked());
-        props.setTimeOff(switchOffTime.getText().toString());
-        props.setTimeOn(switchOnTime.getText().toString());
-        props.save(getApplicationContext());
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        switch (key) {
+            case "activate.simcard": {
+                TelephonyManager tMgr = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+                String simCard = tMgr.getSimSerialNumber();
+                if (simCard == null || simCard.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Unable to retrieve SIM Card Serial!", Toast.LENGTH_LONG).show();
+                    //activeOnSIMCard.setChecked(false);
+                }
+                System.out.println(key);
+            }
+        }
     }
 
     @Override
-    protected void onDestroy() {
-        persist();
-        super.onDestroy();
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        System.out.println(preference);
+        return true;
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).registerOnSharedPreferenceChangeListener(this);
+    }
+
 
     @Override
     protected void onPause() {
         super.onPause();
-        persist();
+        PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).unregisterOnSharedPreferenceChangeListener(this);
     }
+
 }
