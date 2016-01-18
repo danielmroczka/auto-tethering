d375360b8a8a8296763c80fee7f375d9f6f66777
daniel <daniel.mroczka@gmail.com>
2016-01-18 20:11:23 +0100

    fix problem with autostart tethering by widget upon system reboot

--- auto-tethering-app/src/main/java/com/labs/dm/auto_tethering/receiver/TetheringWidgetProvider.java
+++ auto-tethering-app/src/main/java/com/labs/dm/auto_tethering/receiver/TetheringWidgetProvider.java
@@ -22,15 +22,12 @@ public class TetheringWidgetProvider extends AppWidgetProvider {
         int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
         for (int widgetId : allWidgetIds) {
             RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout_off);
-            Intent intent = new Intent(context, TetheringWidgetProvider.class);
-            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
-            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+            Intent intent = new Intent(context, WidgetService.class);
+            PendingIntent pendingIntent = PendingIntent.getService(context, 0, intent, 0);
+
             remoteViews.setOnClickPendingIntent(R.id.widget_button, pendingIntent);
             appWidgetManager.updateAppWidget(widgetId, remoteViews);
         }
-
-        context.startService(new Intent(context, WidgetService.class));
     }
-
 }
