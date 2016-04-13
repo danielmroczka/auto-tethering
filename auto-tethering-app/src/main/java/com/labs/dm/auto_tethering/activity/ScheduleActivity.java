f0191cd837545e3f0b3b26575203b868d61649da
daniel.mroczka@gmail.com <Kotulanka23>
2016-04-13 11:02:55 +0200

    Cleanup

--- auto-tethering-app/src/main/java/com/labs/dm/auto_tethering/activity/ScheduleActivity.java
+++ auto-tethering-app/src/main/java/com/labs/dm/auto_tethering/activity/ScheduleActivity.java
@@ -9,7 +9,6 @@ import android.widget.Button;
 import android.widget.TimePicker;
 import android.widget.Toast;
 import android.widget.ToggleButton;
-
 import com.labs.dm.auto_tethering.R;
 import com.labs.dm.auto_tethering.db.Cron;
 import com.labs.dm.auto_tethering.db.DBManager;
@@ -17,6 +16,8 @@ import com.labs.dm.auto_tethering.db.DBManager;
 public class ScheduleActivity extends Activity {
 
     private DBManager db;
+    private TimePicker timeOff;
+    private TimePicker timeOn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,21 +45,21 @@ public class ScheduleActivity extends Activity {
             }
         });
 
-        ((TimePicker) findViewById(R.id.timeOff)).setIs24HourView(DateFormat.is24HourFormat(this));
-        ((TimePicker) findViewById(R.id.scheduleTimeOn)).setIs24HourView(DateFormat.is24HourFormat(this));
+        timeOff = (TimePicker) findViewById(R.id.scheduleTimeOff);
+        timeOn = (TimePicker) findViewById(R.id.scheduleTimeOn);
+        timeOff.setIs24HourView(DateFormat.is24HourFormat(this));
+        timeOn.setIs24HourView(DateFormat.is24HourFormat(this));
     }
 
     private void insertSchedule() {
-        TimePicker timeOff = (TimePicker) findViewById(R.id.timeOff);
-        TimePicker timeOn = (TimePicker) findViewById(R.id.scheduleTimeOn);
-        ToggleButton[] dayweek = new ToggleButton[7];
+        ToggleButton[] daysOfWeek = new ToggleButton[7];
 
         int[] buttons = {R.id.btnMonday, R.id.btnTuesday, R.id.btnWednesday, R.id.btnThursday, R.id.btnFriday, R.id.btnSaturday, R.id.btnSunday};
 
         int mask = 0;
         for (int day = 0; day < 7; day++) {
-            dayweek[day] = (ToggleButton) findViewById(buttons[day]);
-            if (dayweek[day].isChecked()) {
+            daysOfWeek[day] = (ToggleButton) findViewById(buttons[day]);
+            if (daysOfWeek[day].isChecked()) {
                 mask += Math.pow(2, day);
             }
         }
