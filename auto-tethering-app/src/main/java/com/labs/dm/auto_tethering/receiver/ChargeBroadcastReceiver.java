46cdcabde90865edca59d344399c1fe51c6252eb
danielmroczka <daniel.mroczka@gmail.com>
2016-06-22 16:53:29 +0200

    Experimental - bluetooth activation

--- auto-tethering-app/src/main/java/com/labs/dm/auto_tethering/receiver/ChargeBroadcastReceiver.java
+++ auto-tethering-app/src/main/java/com/labs/dm/auto_tethering/receiver/ChargeBroadcastReceiver.java
@@ -7,8 +7,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.util.Log;
-import android.widget.Toast;
-
 import com.labs.dm.auto_tethering.TetherInvent;
 import com.labs.dm.auto_tethering.service.ServiceHelper;
 import com.labs.dm.auto_tethering.service.TetheringService;
@@ -19,25 +17,25 @@ public class ChargeBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        String action = intent.getAction();
         ServiceHelper helper = new ServiceHelper(context);
         Intent usbIntent = null;
+        Log.i(TAG, intent.getAction());
+        switch (intent.getAction()) {
+            case Intent.ACTION_POWER_CONNECTED: {
+                usbIntent = new Intent(TetherInvent.USB_ON);
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+                if (prefs.getBoolean("usb.internet.start.service", false) && !helper.isServiceRunning(TetheringService.class)) {
+                    Intent serviceIntent = new Intent(context, TetheringService.class);
+                    serviceIntent.putExtra("usb.on", true);
+                    context.startService(serviceIntent);
+                }
 
-        if (action.equals(Intent.ACTION_POWER_CONNECTED)) {
-            usbIntent = new Intent(TetherInvent.USB_ON);
-            Log.i(TAG, "onConnect");
-            //Toast.makeText(context, "USB on", Toast.LENGTH_LONG).show();
-            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-            if (prefs.getBoolean("usb.internet.start.service", false) && !helper.isServiceRunning(TetheringService.class)) {
-                Intent serviceIntent = new Intent(context, TetheringService.class);
-                serviceIntent.putExtra("usb.on", true);
-                context.startService(serviceIntent);
             }
-
-        } else if (action.equals(Intent.ACTION_POWER_DISCONNECTED) && helper.isSharingWiFi()) {
-            usbIntent = new Intent(TetherInvent.USB_OFF);
-            Log.i(TAG, "onDisconnect");
-            Toast.makeText(context, "USB off", Toast.LENGTH_LONG).show();
+            case Intent.ACTION_POWER_DISCONNECTED: {
+                if (helper.isSharingWiFi()) {
+                    usbIntent = new Intent(TetherInvent.USB_OFF);
+                }
+            }
         }
 
         if (usbIntent != null) {
