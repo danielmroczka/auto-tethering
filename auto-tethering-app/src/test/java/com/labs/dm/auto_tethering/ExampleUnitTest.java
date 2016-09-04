472f25a825e64f19c5cffb8d49fdc15f72d90e7c
daniel <daniel.mroczka@gmail.com>
2016-09-05 01:47:40 +0200

    Cleanup

--- auto-tethering-app/src/test/java/com/labs/dm/auto_tethering/ExampleUnitTest.java
+++ auto-tethering-app/src/test/java/com/labs/dm/auto_tethering/ExampleUnitTest.java
@@ -2,13 +2,17 @@ package com.labs.dm.auto_tethering;
 
 import android.content.Context;
 import android.content.Intent;
+
 import com.labs.dm.auto_tethering.receiver.BootCompletedReceiver;
 import com.labs.dm.auto_tethering.service.TetheringService;
+
 import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
@@ -26,6 +30,7 @@ public class ExampleUnitTest {
         BootCompletedReceiver receiver = mock(BootCompletedReceiver.class);
         Context context = mock(Context.class);
         TetheringService service = mock(TetheringService.class);
+
         Intent intent = new Intent(Intent.ACTION_BOOT_COMPLETED);
         receiver.onReceive(context, intent);
         verify(service, atLeastOnce());
